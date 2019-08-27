5491ea945960da7f33eb71de3b2af3a106295547
donjmi <jldonne@gmail.com>
2019-08-28 00:05:04 +0200

    ajout namespace

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,40 +1,45 @@
 <?php
 
-require_once('model/PostManager.php');
-require_once('model/CommentManager.php');
+require('model/frontend.php');
 
 function listPosts()
 {
    
-   	$PostManager = new  PostManager();
-	$posts = $PostManager->getBillets();
+	$posts = getBillets();
 
 	require('view/frontend/listPostsView.php');
 }
 
 function post()
 {
-
-	$PostManager = new  PostManager();
-	$CommentManager = new CommentManager();
-
-
-	$post = $PostManager->viewPost($_GET['id']);
-  	$ncomment = $CommentManager->nbComment($_GET['id']);
-	$ReqComment = $CommentManager->listComments($_GET['id']);
-	   
+    $post = viewPost($_GET['id']);
+    $ncomment = nbComment($_GET['id']);
+    //$insert = addPosts();
+    $ReqComment = listComments($_GET['id']);
+    
     require('view/frontend/postView.php');
 }
+
+/*
+function addComment()
+{
+
+	$insert = addPosts($postId, $author, $comment);
+
+if ($insert === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}*/
 
 
 function addComment($postId, $author, $comment)
 {
-   
-	$CommentManager = new CommentManager();
-    $insert = $CommentManager->postComment($postId, $author, $comment);
+    $insert = postComment($postId, $author, $comment);
 
-    if ($insert === false) 
-    {
+    if ($insert === false) {
         die('Impossible d\'ajouter le commentaire !');
     }
     else {
